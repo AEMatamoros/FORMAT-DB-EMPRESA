aa7a3c467af9a0b7e398058eba44aadd8e240f4a
Alexis Matamoros <alexis.matamoros@gistmo.com>
2021-10-11 16:13:33 -0600

    Cambio en la exportacion del CSV

--- src/index.js
+++ src/index.js
@@ -1,30 +1,10 @@
 const xlsxFile = require('read-excel-file/node')
 const fs = require('fs')
+//CSV
+const createCsvWriter = require('csv-writer').createArrayCsvWriter
 
-//data:text/csv;charset=utf-8,
-let csvFile = ''
-
-xlsxFile('../EMPRESAS.xlsx').then((rows) => {
-  /*
-        0-cod_registro
-        1-ciudad	
-        2-matricula
-        3-socios	
-        4-empresa	
-        5-primer_nombre	
-        6-segundo_nombre	
-        7-primer_apellido	
-        8-segundo_apellido	
-        9-finalidad	
-        10-denominacion	
-        11-tomo	
-        12-fecha	
-        13-PDF
-    */
-  //init
-  let idCount = 0
-
-  let fileRow = [
+const csvWriter = createCsvWriter({
+  header: [
     'Matricula',
     'Tipo de Empresa',
     'Razon Social',
@@ -41,11 +21,18 @@ xlsxFile('../EMPRESAS.xlsx').then((rows) => {
     'Identidad',
     'Nacionalidad',
     'Aporte',
-  ].join(',')
+  ],
+  path: 'docs/empresas.csv',
+  encoding: 'utf8',
+})
 
-  csvFile += fileRow + '\r\n'
+let csvFile = ''
+let records = []
+xlsxFile('../EMPRESAS.xlsx').then((rows) => {
+  //init
+  let idCount = 0
 
-  delete rows[0];
+  delete rows[0]
   //Modificar
   rows.forEach((row) => {
     let newFileRow = []
@@ -53,85 +40,88 @@ xlsxFile('../EMPRESAS.xlsx').then((rows) => {
     let ciudad = ''
     let camara = ''
     let tipoEmpresa = ''
+    let code = ''
     idCount++
 
     //Modifi Dir
-    if(row[4]!=='No hay información' && row[4]!=='No hay empresa'){
-    if (row[1] === 'TGU') {
-      depa = 'Francisco Morazan'
-      ciudad = 'Tegucigalpa'
-      camara = 'Cámara de Comercio e Industrias de Tegucigalpa'
-    } else if (row[1] === 'SPS') {
-      depa = 'San Pedro Sula'
-      ciudad = 'Cortes'
-      camara = 'Cámara de Comercio e Industrias de Puerto Cortés'
-    }
-    //Modifi Razon Social (SA | SA)$
-    if (typeof row[4] !== 'object' && typeof row[4] !== 'number' ) {
-      if (
-        row[4].includes(' SA ') ||
-        row[4].includes(' SOCIEDAD ANONIMA ') ||
-        row[4].includes(' SOCIEDAD ANONIMA') ||
-        row[4].includes('SOCIEDAD ANONIMA ') ||
-        row[4].includes(' S.A.') ||
-        row[4].includes(' S A ') ||
-        row[4].includes('S A ')
-      ) {
-        tipoEmpresa = 'SA'
-      } else if (
-        row[4].match('RESPONSABILIDAD LIMITADA') ||
-        row[4].match('S de RL') ||
-        row[4].match('S de R L') ||
-        row[4].match('RL') ||
-        row[4].match('R L')
-      ) {
-        tipoEmpresa = 'SDRL'
-      } else {
-        tipoEmpresa = 'CI'
+    if (row[4] !== 'No hay información' && row[4] !== 'No hay empresa') {
+      if (row[1] === 'TGU') {
+        depa = 'Francisco Morazan'
+        ciudad = 'Tegucigalpa'
+        camara = 'Cámara de Comercio e Industrias de Tegucigalpa'
+        code = 'TGU'
+      } else if (row[1] === 'SPS') {
+        depa = 'San Pedro Sula'
+        ciudad = 'Cortes'
+        camara = 'Cámara de Comercio e Industrias de Puerto Cortés'
+        code = 'SPS'
       }
-    }
-    //Asign
-    newFileRow[0] = row[2]; //Matricula
-    newFileRow[1] = tipoEmpresa; //T Empresa
-    //console.log(typeof(row[4]))
-    typeof(row[4])=='object' || typeof(row[4])=='number' ? newFileRow[2] = row[4] : newFileRow[2] = row[4].replace(',',' '); //R Social
-    typeof(row[4])=='object' || typeof(row[4])=='number' ? newFileRow[3] = row[4] :  newFileRow[3] = row[4].replace(',',' ');//Nombre Empresa
+      //Modifi Razon Social (SA | SA)$
+      if (typeof row[4] !== 'object' && typeof row[4] !== 'number') {
+        if (
+          row[4].includes(' SA ') ||
+          row[4].includes(' SOCIEDAD ANONIMA ') ||
+          row[4].includes(' SOCIEDAD ANONIMA') ||
+          row[4].includes('SOCIEDAD ANONIMA ') ||
+          row[4].includes(' S.A.') ||
+          row[4].includes(' S A ') ||
+          row[4].includes('S A ')
+        ) {
+          tipoEmpresa = 'SA'
+        } else if (
+          row[4].match('RESPONSABILIDAD LIMITADA') ||
+          row[4].match('S de RL') ||
+          row[4].match('S de R L') ||
+          row[4].match('RL') ||
+          row[4].match('R L')
+        ) {
+          tipoEmpresa = 'SDRL'
+        } else {
+          tipoEmpresa = 'CI'
+        }
+      }
+      //Asign
+      newFileRow[0] = code + row[2] //Matricula
+      newFileRow[1] = tipoEmpresa //T Empresa
+      //console.log(typeof(row[4]))
+      typeof row[4] == 'object' || typeof row[4] == 'number'
+        ? (newFileRow[2] = row[4])
+        : (newFileRow[2] = row[4].replace(',', ' ')) //R Social
+      typeof row[4] == 'object' || typeof row[4] == 'number'
+        ? (newFileRow[3] = row[4])
+        : (newFileRow[3] = row[4].replace(',', ' ')) //Nombre Empresa
 
-    newFileRow[4] = depa //Departamento
-    newFileRow[5] = ciudad //Ciudad
-    newFileRow[6] = camara //Camara de comercio
-    //console.log(typeof(row[7]))
-    typeof(row[7])=='object' || typeof(row[7])=='number' ? newFileRow[7] = row[7] : newFileRow[7] = row[7].replace(',','')//Actividad
+      newFileRow[4] = depa //Departamento
+      newFileRow[5] = ciudad //Ciudad
+      newFileRow[6] = camara //Camara de comercio
+      //console.log(typeof(row[7]))
 
-    newFileRow[8] = '0000-0000' //Telefono
-    newFileRow[9] = 'No especificada' //Direccion
-    newFileRow[10] = 'No especificado' //SitioWeb
-    newFileRow[11] = 'No especificado' //Correo
+      typeof row[9] == 'object' || typeof row[9] == 'number'//Actividad
+        ? (newFileRow[7] = row[9])
+        : (newFileRow[7] = row[9].replace(',', '')) 
 
-    //console.log(typeof(row[3]))
-    typeof(row[3])=='object'|| typeof(row[3])=='number' ? newFileRow[12] = row[3] : newFileRow[12] = row[3].replace(',','')//Socio
+      newFileRow[8] = '0000-0000' //Telefono
+      newFileRow[9] = '' //Direccion
+      newFileRow[10] = '' //SitioWeb
+      newFileRow[11] = '' //Correo
 
-    newFileRow[13] = 'identidad ' + idCount //ID
-    newFileRow[14] = 'Hondureña' //Nacionalidad
-    newFileRow[15] = 1 //Aporte
+      //console.log(typeof(row[3]))
+      typeof row[3] == 'object' || typeof row[3] == 'number'
+        ? (newFileRow[12] = row[3])
+        : (newFileRow[12] = row[3].replace(',', '')) //Socio
 
-    //Covertir en CSV
-    
-    let fileRow = newFileRow.join(',')
-    csvFile += fileRow + '\r\n'
+      newFileRow[13] = 'Temporal ' + idCount //ID
+      newFileRow[14] = 'Hondureña' //Nacionalidad
+      newFileRow[15] = 1 //Aporte
+
+      //Covertir en CSV
+      records.push(newFileRow)
     }
   })
-    //Exportar
-    // Write data in 'newfile.txt' .
-  console.log('Espera un momento');
-  fs.writeFile('empresasFormated.csv', csvFile, (error) => {
-    if (error) {
-      throw err
-    } else {
-      console.log('Saved')
-    }
+  //Exportar
+  console.log('Espera un momento')
+
+  csvWriter.writeRecords(records).then(() => {
+    console.log('...Done')
   })
-  ////console.log(rows[0])
-  ////console.log(rows[1])
-  ////console.log(csvFile)
 })
